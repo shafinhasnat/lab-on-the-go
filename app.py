b713908c92d2015a51c2cf9ec7eb244a6fe38142
shafinhasnat <shafinhasnat97@gmail.com>
2022-09-07 16:54:13 +0600

    ledger added

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, jsonify
+from wsgiref.util import request_uri
+from flask import Flask, jsonify, request
 import os
 import random
 import string
+from pymongo import MongoClient
+import requests
+from datetime import datetime
+
 app = Flask(__name__)
+m = MongoClient("mongodb://poridhimongo:poridhi@36.255.70.114:27017/?authSource=admin", connect=False)
+mongo = m["lab-ledger"]
+
 @app.route("/alive", methods=["GET"])
 def alive():
     resp = jsonify({"ping": "pong"})
@@ -23,9 +31,23 @@ def launchVM():
 
 @app.route("/launch-terminal", methods=["GET"])
 def launchTerminal():
+    token = request.headers.get('Authorization').split()[1]
+    r=requests.get("http://36.255.68.85:5004/user/authenticate", headers={"Authorization": f"Bearer {token}"})
+    print(r.status_code)
+    if r.status_code != 200:
+        resp = jsonify(
+        {
+            "status": "failed",
+            "message": "Unauthorized"
+        }
+        )
+        return resp, 401
+    uid = r.json().get("uid")
     name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
-    port = random.randint(5000, 6000)
+    port = random.randint(5000, 7000)
     os.system('echo "port={} name=termserver-{} ./launchserver.sh" > /hostpipe'.format(port, name))
+
+    mongo.basic_lab.insert_one({"uid": uid, "creation_time": datetime.now()})
     resp = jsonify(
         {
             "status": "success",
